ad4352eeac0268174c05ff7abdf96367376404a4
drylint <drylint@qq.com>
2021-09-17 15:49:46 +0800

    build: 完善配置

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+const path = require('path')
+// 访问绝对路径
+const pathJoin = dir => path.join(__dirname, dir)
 
 module.exports = {
   // pages: getPages('src/pages/*'), // 多页配置
@@ -5,4 +8,36 @@ module.exports = {
   publicPath: './',
   outputDir: 'dist',
   productionSourceMap: false,
+  configureWebpack (config) {
+
+    config.resolve = {
+      extensions: ['.vue', '.tsx', '.ts', '.jsx', '.js', '.json'],
+      alias: {
+        '@': pathJoin('src'),
+      },
+    }
+
+    // 从外部引入的库，比如在 index.html 中引入 cdn 地址
+    config.externals = {
+      // key 表示 import x from 'key'
+      // value 表示外部引入的库暴露的全局变量名
+      'vue': 'Vue',
+      'vue-router': 'VueRouter',
+      'vuex': 'Vuex',
+      'axios': 'axios',
+      'vant': 'vant',
+      'element-ui': 'ELEMENT',
+      'echarts': 'echarts',
+      'BMap': 'BMap',
+      'videojs': 'videojs',
+      'lodash': '_',
+      'qq': 'qq',
+      'wx': 'wx',
+      'callapp-lib': 'CallApp',
+      'xlsx': 'XLSX',
+      'tinymce': 'tinymce',
+      'nprogress': 'NProgress',
+      'compressorjs': 'Compressor',
+    }
+  },
 }
