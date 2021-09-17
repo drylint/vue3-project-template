9afbc5b9a8960d97b9cfd3d3161ba14c90cf4ed8
drylint <drylint@qq.com>
2021-09-17 15:50:14 +0800

    refactor: 项目模板完善

---
+++ src/views/home/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="view-temp">
+    temp
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data () {
+    return {
+    }
+  },
+  created () {
+    // ...
+  },
+  methods: {
+    // ...
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.view-temp {
+  font-size: inherit;
+}
+</style>
