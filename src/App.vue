df8273e5b3744d942b99401e6599fb4bd6e4d2e6
drylint <drylint@qq.com>
2021-09-17 15:49:08 +0800

    refactor(App.vue): 优化结构

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
@@ -12,19 +8,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 
-#nav {
-  padding: 30PX;
-
-  a {
-    color: #2c3e50;
-    font-weight: bold;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
