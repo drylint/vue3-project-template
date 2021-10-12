b434cc3b0c182e68ebfc77664c0d6602e707c0aa
drylint <drylint@qq.com>
2021-10-12 22:19:29 +0800

    feat: 二次封装 van-field

---
+++ src/components/BaseField/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <van-field
+    v-bind="{
+      'placeholder': '请输入',
+      ...$attrs,
+    }"
+  >
+    <!-- slot 透传 -->
+    <template
+      v-for="slotName in Object.keys($slots)"
+      #[slotName]="slotProps"
+    >
+      <slot :name="slotName" v-bind="slotProps || {}" />
+    </template>
+  </van-field>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  inheritAttrs: false,
+})
+</script>
+
+<style lang="scss" scoped>
+.view-temp {
+  font-size: inherit;
+}
+</style>
